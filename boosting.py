05a934e5196a70d47a3613471394d4d03d514002
Lauren Cho <laurencho@ucsb.edu>
2026-03-10 01:33:55 -0700

    fixed boosting

--- boosting.py
+++ boosting.py
@@ -45,9 +45,6 @@ def preprocess_data(X_train, X_test):
     X_train_new = (X_train - mean)/std
     X_test_new = (X_test - mean)/std
 
-    X_train_new = np.hstack([X_train_new, np.ones((X_train_new.shape[0], 1), dtype=X_train_new.dtype)])
-    X_test_new = np.hstack([X_test_new, np.ones((X_test_new.shape[0], 1), dtype=X_test_new.dtype)])
-
     if np.isnan(X_train_new).any() or np.isnan(X_test_new).any():
         raise ValueError("NaNs found after preprocessing — data loading/conversion is wrong.")
   
@@ -57,18 +54,103 @@ def preprocess_data(X_train, X_test):
 class BoostingClassifier:
     """AdaBoost Classifier with weighted linear classifier as weak learner."""
     # initialize our stuff
-    def __init__(self):
-      
+    def __init__(self, T=10):
+        self.T = T
+        self.learners = []
+        self.alphas = []
 
+    # helper function for each of the learners
+    def learner_help(self, X, y, sample_weights):
+        pos_mask = (y == 1)
+        neg_mask = (y == -1)
+
+        X_pos = X[pos_mask]
+        X_neg = X[neg_mask]
+
+        w_pos = sample_weights[pos_mask]
+        w_neg = sample_weights[neg_mask]
+
+        num_features = X.shape[1]
+
+        centroid_pos = np.zeros(num_features)
+        centroid_neg = np.zeros(num_features)
+
+        sum_w_pos = 0.0
+        sum_w_neg = 0.0
+
+        # positive class centroid
+        for i in range(len(X_pos)):
+            sum_w_pos += w_pos[i]
+            for j in range(num_features):
+                centroid_pos[j] += w_pos[i] * X_pos[i][j]
+
+        for j in range(num_features):
+            centroid_pos[j] /= sum_w_pos
+
+        # negative class centroid
+        for i in range(len(X_neg)):
+            sum_w_neg += w_neg[i]
+            for j in range(num_features):
+                centroid_neg[j] += w_neg[i] * X_neg[i][j]
+
+        for j in range(num_features):
+            centroid_neg[j] /= sum_w_neg
+
+        midpoint = (centroid_pos + centroid_neg)/2.0
+        direction = centroid_pos - centroid_neg
+
+        return {"centroid_pos": centroid_pos, "centroid_neg": centroid_neg, "midpoint": midpoint, "direction": direction, }
+
+
+    def learner_predict(self, learner, X):
+        scores = np.dot(X - learner["midpoint"], learner["direction"])
+        return np.where(scores > 0, 1, -1)
+    
     def train(self, X, y):
         """Fit the classifier to training data."""
-        # TODO: Implement
-        raise NotImplementedError
+        # Initialize : w_i = 1/ m for all i = 1,..,m (m = number of training points)
+        m = X.shape[0] # this 
+        w = np.ones(m) / m
+
+        self.learners = []
+        self.alphas = []
+
+        for t in range(self.T):
+            # Train weak learner h_t using weights w
+            learner  = self.learner_help(X, y, w)
+            pred = self.learner_predict(learner, X)
+
+            # Compute weighted error
+            incorrect = (pred != y)
+            epsilon = np.sum(w[incorrect])
+
+            # to ensure no division by zero
+            epsilon = np.clip(epsilon, 1e-10, 1 - 1e-10)
+
+            # Compute model weight
+            alpha = 0.5 * np.log((1 - epsilon) / epsilon)
+
+            # Update sample weights :
+            # - For misclassified points : w_i *= exp ( alpha_t )
+            # - For correctly classified points : w_i *= exp ( - alpha_t )
+            w[incorrect] *= np.exp(alpha)
+            w[~incorrect] *= np.exp(-alpha)
+
+            # Normalize weights so they sum to 1
+            w /= np.sum(w)
+
+            self.learners.append(learner)
+            self.alphas.append(alpha)
 
     def predict(self, X):
         """Predict labels for input samples."""
-        # TODO: Implement
-        raise NotImplementedError
+        final_scores = np.zeros(X.shape[0])
+
+        for alpha, learner in zip(self.alphas, self.learners):
+            pred = self.learner_predict(learner, X)
+            final_scores += alpha * pred
+
+        return np.where(final_scores > 0, 1, -1)
 
 
 def evaluate(y_true, y_pred):
@@ -98,7 +180,7 @@ def run(Xtrain_file: str, Ytrain_file: str, test_data_file: str, pred_file: str)
     # print("X_test", X_test.shape)
 
     # Train
-    model = VotedPerceptron(epochs=3)  # tune epochs
+    model = BoostingClassifier(T=10)  # tune T
     model.train(X_train_p, y_train)
 
     # Predict
